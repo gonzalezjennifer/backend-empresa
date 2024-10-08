ac2609e72fba570137d64272b6305678bfc39969
gonzalezjennifer <62447915+gonzalezjennifer@users.noreply.github.com>
2024-10-08 09:29:02 -0600

    cambios 10 de octubre

--- src/app.js
+++ src/app.js
@@ -3,13 +3,20 @@ import dotenv from 'dotenv'
 import errorHandler from './middlewares/errorHandler.js'
 import rateLimitMiddleware from './middlewares/rateLimit.js'
 import routes from './routes/index.js'
+import cors from 'cors'
 
 dotenv.config()
+
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
 
 const app = express()
 
 app.use(express.json())
 app.use(rateLimitMiddleware)
+app.use(cors(corsOptions))
 app.use('/api/v1', routes)
 app.use(errorHandler)
 
